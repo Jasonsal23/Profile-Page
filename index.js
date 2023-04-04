31e958fe0db4887e3d077c75922a2d44267ce36a
Jason <jason@Jasons-MacBook-Air.local>
2023-04-03 19:48:33 -0700

    update

--- index.js
+++ index.js
@@ -13,10 +13,23 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
+let descHeadline = document.createElement('h3');
+descHeadline.innerText ='Description:';
+dogDetails.append(descHeadline);
+
+let descParagraph = document.createElement('p');
+descParagraph.innerText = "This gentle dog is aloof toward her owner, and never comes when called."
++" She always acts as though any stranger she meets will harm her, and"
++" dislikes other animals."
+dogDetails.append(descParagraph);
+
+let feedingTimesHeadline = document.createElement('h3');
+feedingTimesHeadline.innerText= 'Feeding Times:';
+dogDetails.append(feedingTimesHeadline);
 
